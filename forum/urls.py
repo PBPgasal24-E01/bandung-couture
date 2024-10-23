c1286b0266a1a69f5e143e9db14d9e622ece4987
maskrio <yosemaranata@gmail.com>
2024-10-23 15:21:34 +0700

    add child forums, add edit delete read

--- forum/urls.py
+++ forum/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
-from forum.views import show_forum_page, show_root_json, add_forum_entry_ajax, edit_forum, delete_forum
+from forum.views import show_forum_page, show_root_json, add_forum_entry_ajax, edit_forum, delete_forum, show_forum_page_id, show_json_by_id, show_json_childs_by_id, show_json
 from django.shortcuts import redirect
 
 app_name = 'forum'
 
 urlpatterns = [
     path('', lambda request: redirect('forum:show'), name='default'),
-    path('show', show_forum_page, name='show'),
-    path('show_root_json', show_root_json, name='show_root_json'),
-    path('add_forum_entry_ajax', add_forum_entry_ajax, name='add_forum_entry_ajax'),
-    path('edit_forum', edit_forum, name="edit_forum"),
-    path('delete_forum', delete_forum, name="delete_forum")
+    path('show/', show_forum_page, name='show'),
+    path('show/<uuid:id>/', show_forum_page_id, name='show_id'),
+    path('show_root_json/', show_root_json, name='show_root_json'),
+    path('show_json/', show_json, name='show_json'),
+    path('show_json_by_id/<uuid:id>/', show_json_by_id, name='show_json_by_id'),
+    path('show_json_childs_by_id/<uuid:id>/', show_json_childs_by_id, name='show_json_childs_by_id'),
+    path('add_forum_entry_ajax/', add_forum_entry_ajax, name='add_forum_entry_ajax'),
+    path('edit_forum/', edit_forum, name="edit_forum"),
+    path('delete_forum/', delete_forum, name="delete_forum"),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from forum.models import Forum
 from django.http import HttpResponse
 from django.core import serializers
@@ -7,14 +7,75 @@ from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 from django.http import JsonResponse
 from forum.forms import ForumEntryForm
+from django.contrib.auth.decorators import login_required
+from datetime import datetime, timedelta
+from django.utils.timezone import now
 
 
 # Create your views here.
+@login_required(login_url='/account/login')
 def show_forum_page(request):
     return render(request, "forum_page.html", {})
 
-def show_root_json(request) :
-    data = Forum.objects.filter(parent__isnull=True)
+@login_required(login_url='/account/login')
+def show_forum_page_id(request,id):
+    current_user = request.user
+    context = {
+        'pk': id
+    }
+    return render(request, "forum_page_id.html", context)
+
+def show_json(request):
+    data = Forum.objects.all()
+    current_user = request.user
+    forum_entries = []
+    
+    for forum_data in data:
+        if( forum_data.parent == None) :
+            parent = "None"
+        else :
+            parent = forum_data.parent.id
+        entry = {
+            'pk':forum_data.pk,
+            'fields':{
+                'id': forum_data.user.id,
+                'title': forum_data.title,
+                'details': forum_data.details,
+                'time': time_since(forum_data.time),  
+                'username': forum_data.user.username,  
+                'parent': parent,
+                'is_author': (current_user.id == forum_data.user.id),
+            }
+        }
+        
+        forum_entries.append(entry)
+
+    # Return the data as JSON
+    return JsonResponse(forum_entries, safe=False)
+
+def show_json_by_id(request,id) :
+    try :
+        forum_data = Forum.objects.get(pk=id)
+    except : 
+        return HttpResponse("NOT FOUND", status=404)
+    current_user = request.user
+    
+    entry = {
+        'pk':forum_data.pk,
+        'fields':{
+            'id': forum_data.user.id,
+            'title': forum_data.title,
+            'details': forum_data.details,
+            'time': time_since(forum_data.time),  
+            'username': forum_data.user.username,  
+            'is_author': (current_user.id == forum_data.user.id),
+        }
+    }
+
+    return JsonResponse(entry, safe=False)
+
+def show_json_childs_by_id(request,id) :
+    data = Forum.objects.filter(parent=id)
     current_user = request.user
     forum_entries = []
     
@@ -26,14 +87,36 @@ def show_root_json(request) :
                 'id': forum_data.user.id,
                 'title': forum_data.title,
                 'details': forum_data.details,
-                'time': forum_data.time,  
+                'time': time_since(forum_data.time),  
                 'username': forum_data.user.username,  
                 'is_author': (current_user.id == forum_data.user.id),
             }
         }
         
+        forum_entries.append(entry)
+
+    # Return the data as JSON
+    return JsonResponse(forum_entries, safe=False)
+
+def show_root_json(request) :
+    data = Forum.objects.filter(parent__isnull=True).order_by("-time")
+    current_user = request.user
+    forum_entries = []
+    
+    for forum_data in data:
         
-            
+        entry = {
+            'pk':forum_data.pk,
+            'fields':{
+                'id': forum_data.user.id,
+                'title': forum_data.title,
+                'details': forum_data.details,
+                'time': time_since(forum_data.time),  
+                'username': forum_data.user.username,  
+                'is_author': (current_user.id == forum_data.user.id),
+            }
+        }
+        
         forum_entries.append(entry)
 
     # Return the data as JSON
@@ -44,42 +127,66 @@ def show_root_json(request) :
 def add_forum_entry_ajax(request):
     title = strip_tags(request.POST.get("title"))
     details = strip_tags(request.POST.get("details")) 
-    parent = request.POST.get("parent")
-    user = request.user
-    new_forum = Forum(
-        title=title, details=details,
-        user=user,
-    )
-    new_forum.save()
 
+    parentpk = request.POST.get("parent")
+     
+    user = request.user
+    
+    if(parentpk != None) :
+        try :
+            parent = Forum.objects.get(pk=parentpk)
+        except : 
+            return HttpResponse(b"NOT FOUND", status=404)
+        new_forum = Forum(
+            title=title, details=details,
+            user=user,
+            parent=parent
+        )
+        new_forum.save()
+    else : 
+        new_forum = Forum(
+            title=title, details=details,
+            user=user,
+        )
+        new_forum.save()
     return HttpResponse(b"CREATED", status=201)
 
 @csrf_exempt
 @require_POST
-def edit_forum(request): 
+def edit_forum(request):
     forum = Forum.objects.get(pk=request.POST.get("pk"))
-    print("successfully get forum")
-    
     form = ForumEntryForm(request.POST or None, instance=forum)
-    print("successfully get form")
 
-    if form.is_valid() and request.method == "POST":
-        
-        if(request.user != forum.user) :
-            return HttpResponse(b"Failed, unauthorized", status=401)
-        
-        form.save()
-        return HttpResponse(b"EDITED", status=201)
     
-    return HttpResponse(b"Failed", status=400)
+    if(request.user != forum.user) :
+        return HttpResponse(b"Failed, unauthorized", status=401)
+    
+    form.save()
+    return HttpResponse(b"EDITED", status=201)
+
 
 @csrf_exempt
 @require_POST
 def delete_forum(request):
+    print(request.POST.get("pk"))
     forum = Forum.objects.get(pk = request.POST.get("pk"))
-    # Hapus forum
+
     if(request.user != forum.user) :
         return HttpResponse(b"Failed, unauthorized", status=401)
     
     forum.delete()
     return HttpResponse(b"DELETED", status=201) 
+
+
+def time_since(time):
+    delta = now() - time
+
+    if delta < timedelta(hours=1):
+        return "recent"
+    elif delta < timedelta(days=1):
+        hours = delta.seconds // 3600
+        return f"{hours}h ago"
+    else:
+        days = delta.days
+        return f"{days}d ago"
+    
